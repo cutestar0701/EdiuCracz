a8a606eecb488d2b9f74bc15aba8698a5fc4f8fa
DESKTOP-P3C3FEB\kgh0701 <cutestar0701@gmail.com>
2022-09-15 10:51:39 +0900

    my port

---
+++ project_get_dependencies.bat
@@ -0,0 +1,46 @@
+#text to speech
+
+#git clone https://github.com/RicardoValladares/Texto_a_Voz.git
+#git clone https://github.com/PriyankaGR/Microsoft-Speech-to-Text-App.git
+#git clone https://github.com/OskiSolutions/SpeechSmartTV.git
+#git clone https://github.com/cewitte/Pabx.git
+#git clone https://github.com/Toxoid49b/C-SAPI.git
+#git clone https://github.com/ActiveNick/Unity-Text-to-Speech.git
+#git clone https://github.com/brookhong/tts.git
+#git clone https://github.com/pkrumins/speak-text-files-to-wav.git
+#git clone https://github.com/yashasvigirdhar/MS-SAPI-demo.git
+#git clone https://github.com/mamarche/UnitySpeechManager.git
+#git clone https://github.com/GuojunZhang-MTC/Pronunciation-Assessment-tool.git
+#git clone https://github.com/kaldi-asr/kaldi.git
+#git clone https://github.com/jimbozhang/speechocean762.git
+
+#reversing
+#git clone https://github.com/onethawt/idaplugins-list.git
+#git clone https://github.com/cutestar0701/freeCodeCamp.git
+git clone https://github.com/google/binexport.git
+git clone https://github.com/mitre-attack/attack-navigator.git
+git clone https://github.com/strazzere/android-unpacker.git
+git clone https://github.com/wheelert/FlexNetParse.git
+git clone https://github.com/michalrames/lmadmin_monitor.git
+git clone https://github.com/CharlesWB/LicenseStatus.git
+
+
+# NLP & GM
+git clone https://github.com/microsoft/CodeXGLUE.git
+git clone https://github.com/rbavishi/Context2Name.git
+git clone https://github.com/squaresLab/VarCLR.git
+git clone https://github.com/cutestar0701/opennn.git
+git clone https://github.com/google-research/bert.git
+git clone https://github.com/yuewang-cuhk/awesome-programming-language-pretraining-papers.git
+git clone https://github.com/amanaligit/agar.io-clone.git
+git clone https://github.com/OgarProject/Ogar.git
+git clone https://github.com/huytd/agar.io-clone.git
+
+
+#slide show
+git clone https://github.com/lief-project/LIEF.git
+git clone https://github.com/evilsocket/arminject.git
+git clone https://github.com/jdan/cleaver.git
+git clone https://github.com/ksky521/nodeppt.git
+git clone https://github.com/alvarotrigo/fullPage.js.git
+git clone https://github.com/balenablocks/photo-gallery.git
